b50ae85b9673ed234d371106aadb78e16b3a30f8
Alex Peña <attn.alex.p@gmail.com>
2020-11-10 14:43:45 -0800

    completed fibonacci & lucas nth values

--- math_series/series_module.py
+++ math_series/series_module.py
@@ -1,7 +1,19 @@
 def fibonacci(n):
-    return 1
+    if n<0: 
+        return None 
+    elif n==0: 
+        return 0
+    elif n==1:
+        return 1
+    else: 
+        return fibonacci(n-1)+fibonacci(n-2) 
 
 def lucas(n):
-    return 2
-
-    
+    if n<0: 
+        return None 
+    elif n==0: 
+        return 2
+    elif n==1:
+        return 1
+    else: 
+        return lucas(n-1)+lucas(n-2) 
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,7 +1,67 @@
-from math_series.series_module import fibonacci
+from math_series.series_module import fibonacci, lucas
+
 
 def test_import():
     assert fibonacci
-
-def lucas():
     assert lucas
+
+def test_fibonacci_0():
+    actual = fibonacci(0)
+    expected = 0
+    assert actual == expected
+
+def test_fibonacci_1():
+    actual = fibonacci(1)
+    expected = 1
+    assert actual == expected
+
+def test_fibonacci_2():
+    actual = fibonacci(2)
+    expected = 1
+    assert actual == expected
+
+def test_fibonacci_3():
+    actual = fibonacci(3)
+    expected = 2
+    assert actual == expected
+
+def test_fibonacci_4():
+    actual = fibonacci(4)
+    expected = 3
+    assert actual == expected
+
+def test_fibonacci_negative():
+    actual = fibonacci(-3)
+    expected = None
+    assert actual == expected
+
+def test_lucas_0():
+    actual = lucas(0)
+    expected = 2
+    assert actual == expected
+
+def test_lucas_1():
+    actual = lucas(1)
+    expected = 1
+    assert actual == expected
+
+def test_lucas_2():
+    actual = lucas(2)
+    expected = 3
+    assert actual == expected
+
+def test_lucas_3():
+    actual = lucas(3)
+    expected = 4
+    assert actual == expected
+
+def test_lucas_4():
+    actual = lucas(4)
+    expected = 7
+    assert actual == expected
+
+def test_lucas_negative():
+    actual = lucas(-3)
+    expected = None
+    assert actual == expected
+
