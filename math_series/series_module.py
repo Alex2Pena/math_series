e6ad1adf48cc25439d11eb3bad368559e71d5760
Alex Peña <attn.alex.p@gmail.com>
2020-11-10 15:47:08 -0800

    added tests

--- math_series/series_module.py
+++ math_series/series_module.py
@@ -1,10 +1,8 @@
 def fibonacci(n):
     if n < 0: 
         return None 
-    elif n == 0: 
-        return 0
-    elif n == 1:
-        return 1
+    elif n <= 1: 
+        return n
     else: 
         return fibonacci(n - 1) + fibonacci(n - 2) 
 
